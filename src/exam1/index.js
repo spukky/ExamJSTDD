6c96ea43d59e24811be8cc4e8a5c0c8645970df8
spukky <darknessknight.darkness@gmail.com>
2021-04-08 11:03:52 +0700

    Update Roman to Integer

--- src/exam1/index.js
+++ src/exam1/index.js
@@ -1,7 +1,29 @@
 function roman(value) {
-    return '4';
+    const sym = { 
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let result = 0;
+    for(let i = 0; i<value.length; i++){
+                const cur = sym[value[i]] ;
+        const next = sym[value[i+1]];
+        if(cur < next){
+            result += next - cur; // IV -> 5 - 1 = 4
+        }
+        else{
+            result += cur;
+        }
+    }
+    
+    return result ;
+    
 }
-
+// console.log(roman("III"));
 module.exports = {
     roman: roman,
 };
