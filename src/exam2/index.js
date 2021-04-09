24f46a8b763049c04b340e338cb14d7d27112d6a
spukky <darknessknight.darkness@gmail.com>
2021-04-09 13:01:03 +0700

    Update 2.1 Series call back

--- src/exam2/index.js
+++ src/exam2/index.js
@@ -1,13 +1,27 @@
+//2.1
+
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var results = [];
 
-
-function async(arg, callback) {
-    setTimeout(function () { callback(arg); }, 1200 * Math.random());
+function timeout() {
+    return new Promise(resolve => setTimeout(resolve, 1200 * Math.random()));
 }
 
-for (i = 0; i < a.length; i++) {
-    async(a[i], (result) => {
-        console.log(result);
-    })
+async function async(arg, callback) {
+    await timeout();
+    await callback(arg);
 }
+
+async function test() {
+    for (i = 0; i < a.length; i++) {
+        await async(a[i], (result) => {
+            if (result % 3 == 0) {
+
+            }
+            console.log(result);
+        })
+
+    }
+}
+
+test();
